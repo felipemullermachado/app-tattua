9b0deb54bdd49fdc7842e792c2e98c78ddeabe6d
Felipe Muller Machado <felipe.machado@dotgroup.com.br>
2023-06-06 10:32:21 -0300

    implementa tela Edita Cliente

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -1,5 +1,7 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Home } from "../screens/Home";
+import { EditaCliente } from "../screens/Editar/EditaCliente";
+
 
 const { Screen, Navigator } = createNativeStackNavigator();
 //Arquivo que configura as rotas das telas existentes
@@ -7,6 +9,7 @@ export const StackRoutes = () => {
   return (
     <Navigator screenOptions={{ headerShown: false }}>
       <Screen name="Home" component={Home} />
+      <Screen name="EditaCliente" component={EditaCliente} />
     </Navigator>
   );
 };
