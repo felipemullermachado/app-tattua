9eb063ec72a723c33d5b4130b62471bea99095fe
Felipe Muller Machado <felipe.machado@dotgroup.com.br>
2023-06-06 10:36:31 -0300

    implementa tela Perfil do cliente

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -1,7 +1,7 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Home } from "../screens/Home";
 import { EditaCliente } from "../screens/Editar/EditaCliente";
-
+import { Perfil } from "../screens/Perfil";
 
 const { Screen, Navigator } = createNativeStackNavigator();
 //Arquivo que configura as rotas das telas existentes
@@ -9,6 +9,7 @@ export const StackRoutes = () => {
   return (
     <Navigator screenOptions={{ headerShown: false }}>
       <Screen name="Home" component={Home} />
+      <Screen name="Perfil" component={Perfil} />
       <Screen name="EditaCliente" component={EditaCliente} />
     </Navigator>
   );
