0fc1238b8c66e836d65336c8729ddf1cd9f25193
Felipe Muller Machado <felipe.machado@dotgroup.com.br>
2023-06-08 17:50:00 -0300

    rotas para as telas 'pedido' e 'edita pedido'

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -2,6 +2,8 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Home } from "../screens/Home";
 import { EditaCliente } from "../screens/Editar/EditaCliente";
 import { Perfil } from "../screens/Perfil";
+import { Pedido } from "../screens/Pedido";
+import { EditaPedido } from "../screens/Editar/EditaPedido";
 
 const { Screen, Navigator } = createNativeStackNavigator();
 //Arquivo que configura as rotas das telas existentes
@@ -11,6 +13,8 @@ export const StackRoutes = () => {
       <Screen name="Home" component={Home} />
       <Screen name="Perfil" component={Perfil} />
       <Screen name="EditaCliente" component={EditaCliente} />
+      <Screen name="Pedido" component={Pedido} />
+      <Screen name="EditaPedido" component={EditaPedido} />
     </Navigator>
   );
 };
